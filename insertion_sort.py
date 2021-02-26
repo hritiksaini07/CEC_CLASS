535571b56f3ef72a357c6f3dbf4fa81ae8228654
Hritik Saini <hritik.saini.rke@gmail.com>
2021-02-26 09:52:41 +0530

    commit

---
+++ insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+l=[int(l) for l in input().split()]
+insertion_sort(l)
+print(l)
